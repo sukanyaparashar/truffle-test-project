b1868bca5b433f5fbbf0698ce99b63811790f01f
Sukanya Parashar <sukanyaparashar111@gmail.com>
2022-12-21 12:56:58 -0700

    Uncomment the development network settings from truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
 module.exports = {
 
   networks: {
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
   },
 
   // Set default mocha options here, use special reporters, etc.
@@ -16,7 +16,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.15" // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.17" // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
